968c862c77af96237f4b08e6c588cfa4932b0507
ikakus <idadiani88@gmail.com>
2014-01-01 16:31:34 +0400

    first commit

---
+++ WpfApplication1/CustomTimer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Windows;
+using Microsoft.Win32;
+
+namespace WpfApplication1
+{
+    internal class CustomTimer
+    {
+        private WorkHours mWorkHours;
+        public CustomTimer()
+        {
+            SystemEvents.SessionSwitch += SystemEvents_SessionSwitch;
+            mWorkHours = new WorkHours(DateTime.Now);
+        }
+
+        private void SystemEvents_SessionSwitch(object sender, SessionSwitchEventArgs e)
+        {
+            switch (e.Reason)
+            {
+                case SessionSwitchReason.SessionLock:
+
+                   
+                    mWorkHours.OnLoggedOut();
+                   // MessageBox.Show("Logged out, Time LoggedI :" + mWorkHours.MTimeSpan.Minutes.ToString());
+                    
+                    break;
+
+                case SessionSwitchReason.SessionUnlock:
+
+                    mWorkHours.OnLoggedIn();
+                   // MessageBox.Show("Logged In");
+                   
+                    break;
+            }
+        }
+    }
+}
---
+++ WpfApplication1/WorkHours.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApplication1
+{
+    class WorkHours
+    {
+
+        private DateTime mStartTime;
+        private DateTime mEndTime;
+        private TimeSpan mTimeSpan;
+
+        public WorkHours()
+        {
+            mStartTime = DateTime.MinValue;
+            mEndTime = DateTime.MinValue;
+            mTimeSpan = new TimeSpan(0,0,0);
+        }
+
+        public WorkHours(DateTime sTime)
+        {
+            mStartTime = sTime;
+            mEndTime = DateTime.MinValue;
+            mTimeSpan = new TimeSpan(0, 0, 0);
+        }
+
+        public WorkHours(DateTime sTime, DateTime eTime)
+        {
+            mStartTime = sTime;
+            mEndTime = eTime;
+            mTimeSpan = new TimeSpan(0, 0, 0);
+        }
+
+        public DateTime MEndTime
+        {
+            get { return mEndTime; }
+            set { mEndTime = value; }
+        }
+
+        public DateTime MStartTime
+        {
+            get { return mStartTime; }
+            set { mStartTime = value; }
+        }
+
+        public TimeSpan MTimeSpan
+        {
+            get { return mTimeSpan; }
+            set { mTimeSpan = value; }
+        }
+
+        public void OnLoggedOut()
+        {
+            MEndTime = DateTime.Now;
+            MTimeSpan += MEndTime - MStartTime;
+        }
+
+        public void OnLoggedIn()
+        {
+            MStartTime = DateTime.Now;
+        }
+    }
+}
